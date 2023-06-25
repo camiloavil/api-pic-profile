c2726a75989b19c94ca930a096575da894046e43
Camilo Avila <camilo.avil@gmail.com>
2023-06-24 21:04:44 -0500

    Fix security access of Users

--- app/routers/userRouter.py
+++ app/routers/userRouter.py
@@ -2,17 +2,30 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi import Body
 from fastapi.responses import JSONResponse
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 #SQLModel
-from sqlmodel import Session
+from sqlmodel import Session, select
 from sqlalchemy.exc import IntegrityError
 # APP
 from app.models.user import User, UserNew, UserLogin, UserFB
-from app.DB.db import get_session
+from app.DB.db import get_session, engine
 # passlib
 from passlib.context import CryptContext
+#jose
+from jose import jwt, JWTError
 # Python
-from typing import Annotated
+from typing import Annotated, Union
+from datetime import datetime, timedelta
+from pydantic import BaseModel
+
+
+SECRET_KEY = "54f12eb620bc1495166793b5d38d29b0468ef97d33c92eb1fcc0a6de916816d7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -22,24 +35,73 @@ users_router = APIRouter()
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
 
-
 def get_password_hash(password: str):
     return pwd_context.hash(password.encode('utf-8'))
 
+def get_user_db(username: str):
+    with Session(engine) as session:
+        statement = select(User).where(User.email == username)
+        user_db = session.exec(statement).first()
 
-def fake_decode_token(token):
-    print (f'fake_decode_token: {token}')
-    return User(username=token + "fakedecoded", email="john@example.com")
+    if user_db is None:
+        return None
+    return user_db
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=5)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail="Could not validate credentials",
+                                          headers={"WWW-Authenticate": "Bearer"},)
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = get_user_db(username=username)
+    if user is None:
+        raise credentials_exception
     return user
 
 @users_router.get(path="/myuser/",
+                  response_model=UserFB,
                   tags=["Trys"])
-async def read_items(current_user: Annotated[User, Depends(get_current_user)]):
-    print(str(current_user))
-    return current_user
+async def info_User(current_user: Annotated[User, Depends(get_current_user)]):
+    """
+    Get information about the current user.
+
+    :return: UserFB - Information about the current user
+    """
+    return UserFB(**current_user.dict())
+
+
+@users_router.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                session: Session = Depends(get_session)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail="Could not validate credentials",
+                                          headers={"WWW-Authenticate": "Bearer"},)
+    user_db = get_user_db(form_data.username)
+
+    if user_db is None:
+        raise credentials_exception
+    if not verify_password(form_data.password, user_db.pass_hash):
+        raise credentials_exception
+    access_token = create_access_token(data={"sub": user_db.email}, 
+                                       expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
 
 @users_router.get(path="/users", 
                  response_model=list[UserFB], 
@@ -77,11 +139,17 @@ def create_user(user: UserNew = Body(...),
     return UserFB(user_id=new_user.user_id, name=user.name, email=user.email)
     # return JSONResponse(status_code=status.HTTP_201_CREATED,content={'message' : 'User added', 'id' : user_db.id, 'username' : user_db.name })
 
-@users_router.post(path="/login",
+@users_router.get(path="/users/me/", 
+                  response_model=UserFB,
+                  tags=["Users"])
+async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
+
+@users_router.post(path="/logintest",
                   response_model=UserFB,
                   tags=["Users"],
                   status_code=status.HTTP_200_OK)
-def login(user: UserLogin = Body(...),
+def loginTest(user: UserLogin = Body(...),
           session: Session = Depends(get_session)):
     user_db = session.query(User).filter(User.email == user.email).first()
     
