e5f88ebb6409e91d0b0cb31c112f04bb84f52752
Camilo Avila <camilo.avil@gmail.com>
2023-07-10 21:40:31 -0500

    chore(Service free picture) define quality of picture an fix code to service module cleaning paths operations

--- app/dependencies/service.py
+++ app/dependencies/service.py
@@ -1,5 +1,4 @@
 # FastAPI
-from fastapi import status, HTTPException
 from fastapi import File
 #APP
 from app.models.user import User
@@ -9,47 +8,74 @@ from ProfilePicMaker.app.models.pictures import BigPic
 from ProfilePicMaker.app.models.colors import Color
 # Python 
 from typing import Optional
+from enum import Enum
 import tempfile
 
 TEMPORARY_DURATION = 5
 
+class NoFaceException(Exception):
+    def __init__(self, message: str) -> None:
+        self.message = message
+
+class QualityType(str, Enum):
+    THUMBNAIL = 'thumbnail'
+    PREVIEW = 'preview'
+    MEDIUM = 'medium'
+    HIGH = 'high'
+    FULLSIZE = 'fullsize'
+
 class MakePicture:
     def __init__(self,user : User) -> None:
         self.user = user
+
+    async def make_user_picture(self, 
+                                pic_file : File, 
+                                Acolor : Color, 
+                                Bcolor : Color, 
+                                BorderColor : Optional[Color] = None,
+                                quality : QualityType = QualityType.PREVIEW,
+                                index : Optional[int] = 0) -> str:
+        print('lets create a picture for a user') 
+        print(self.user)
     
     @staticmethod
     async def make_temp_picture(pic_file : File, 
                                 Acolor : Color, 
                                 Bcolor : Color, 
                                 BorderColor : Optional[Color] = None,
-                                index : Optional[int] = 0):
+                                quality : Optional[QualityType] = QualityType.PREVIEW,
+                                index : Optional[int] = 0) -> str:
         """
-        Create a temporary picture using the provided `pic_file` and apply various image processing operations on it.
+        Creates a temporary picture with the specified parameters.
 
         Parameters:
-            pic_file (File): The input picture file.
-            Acolor (Color): The color of the foreground.
-            Bcolor (Color): The color of the background.
-            BorderColor (Optional[Color]): The color of the border (default: None).
-            index (Optional[int]): The index of the face to process (default: 0).
+            pic_file (File): The picture file to be used.
+            Acolor (Color): The color for the A component.
+            Bcolor (Color): The color for the B component.
+            BorderColor (Optional[Color], optional): The color for the border. Defaults to None.
+            quality (Optional[QualityType], optional): The quality type of the picture. Defaults to QualityType.PREVIEW.
+            index (Optional[int], optional): The index of the face. Defaults to 0.
 
         Returns:
-            str: The path of the processed picture.
+            str: The path of the temporary picture.
         """
-        # Create a temporary file
+
         with tempfile.NamedTemporaryFile(delete=True, suffix=".jpg") as temp_file:
-            # Save the uploaded file to the temporary file
             with open(temp_file.name, "wb") as f:
                 f.write(await pic_file.read())
-            # print(temp_file.name)
             faces = BigPic(temp_file.name).get_faces()
             if(len(faces) == 0):
-                raise HTTPException(
-                    status_code=status.HTTP_409_CONFLICT,
-                    detail= "No Faces detected in the picture 'image/jpeg'",
-                    )
-            print(f'Number of faces detected: {len(faces)} and index: {index}')
-            faces[index].resize(300)
+                raise NoFaceException("No Faces detected in the picture 'image/jpeg'")
+
+            if quality == QualityType.THUMBNAIL:
+                faces[index].resize(150)
+            elif quality == QualityType.PREVIEW:
+                faces[index].resize(300)
+            elif quality == QualityType.MEDIUM:
+                faces[index].resize(600)
+            elif quality == QualityType.HIGH:
+                faces[index].resize(100)
+
             faces[index].removeBG()
             faces[index].addBG(Acolor,Bcolor)
             faces[index].set_contour()
--- app/routers/imagesRouter.py
+++ app/routers/imagesRouter.py
@@ -3,16 +3,15 @@ from fastapi import APIRouter, status, HTTPException, UploadFile
 from fastapi import File, Query, Depends
 from fastapi.responses import FileResponse
 # ProfilePicMaker
-from ProfilePicMaker.app.models.pictures import BigPic
 from ProfilePicMaker.app.models.colors import Color, ColorExamples
 # APP
 from app.models.user import User
-from app.models.picture import Picture
+from app.dependencies.service import QualityType
 from app.dependencies.service import MakePicture
 from app.security.secureuser import get_current_user
+from app.dependencies.service import NoFaceException
 # Python
 from typing import Annotated, Union
-import tempfile
 
 router = APIRouter()
 
@@ -21,12 +20,12 @@ LIMIT_SIZE_USER = 15 # MB
 
 
 @router.post('/example',
-    response_class=FileResponse, 
-    status_code=status.HTTP_201_CREATED)
+             response_class=FileResponse, 
+             status_code=status.HTTP_201_CREATED)
 async def example(pic_file: UploadFile = File(...),
                     index: int = Query(description='Which face in the picture will be used',
-                                       default=1, 
-                                       ge=1, le=10), 
+                                        default=1, 
+                                        ge=1, le=10), 
                     colorA: ColorExamples = Query(description='First Color to use, Default = Black',
                                                   default=ColorExamples.BLACK),
                     colorB: ColorExamples = Query(description='Last Color to use, Default = Black',
@@ -35,7 +34,7 @@ async def example(pic_file: UploadFile = File(...),
                                       Query(description='Border Color to use, Default = None')] = None
                      ):
     """
-    Create a picture Example with specified colors and border.
+    Create a Example picture with specified colors and border.
 
     Parameters:
         pic_file (UploadFile): The picture file to process.
@@ -48,38 +47,28 @@ async def example(pic_file: UploadFile = File(...),
         FileResponse: The processed picture file resized.
     """
     if pic_file.content_type not in ("image/jpeg",):
-        raise HTTPException(
-            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-            detail="Unsupported content type. Supported content types are 'image/jpeg'",
-            )
+        raise HTTPException(status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+                            detail="Unsupported content type. Supported content types are 'image/jpeg'",)
     if pic_file.size > 1024 * 1024 * LIMIT_SIZE_FREE:     # 3MB
-        raise HTTPException(
-            status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
-            detail="File size is too large",
-        )
+        raise HTTPException(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+                            detail=f'File size is too large, the limit is {LIMIT_SIZE_FREE}MB',)
     Acolor = Color.get_color_rgb(colorA.value)
     Bcolor = Color.get_color_rgb(colorB.value)
     BorderColor = None if border is None else Color.get_color_rgb(border.value)
     try:
-        pic_path = await MakePicture.make_temp_picture(pic_file, 
-                                                       Acolor, 
-                                                       Bcolor, 
-                                                       BorderColor, 
-                                                       (index-1))
+        pic_path = await MakePicture.make_temp_picture(pic_file=pic_file, 
+                                                       Acolor=Acolor, 
+                                                       Bcolor=Bcolor, 
+                                                       BorderColor=BorderColor, 
+                                                       index=(index-1))
         return FileResponse(pic_path)
+    except NoFaceException as e:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail= e.message,)
     except Exception as e:
         print(str(e))
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail= "there was an error processing your request. Try again later",
-            )
-
-    # return {"filename": pic_file.filename,
-    #         "format": pic_file.content_type,
-    #         "size": pic_file.size,
-    #         "sizeInBytes": round(len(pic_file.file.read())/1024,ndigits=2),
-    #         "temp_file": str(temp_file.name),}
-
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail= "there was an error processing your request. Try again later",)
 
 @router.post('/mypicture',
     response_class=FileResponse, 
@@ -89,65 +78,68 @@ async def get_my_picture(current_user: Annotated[User, Depends(get_current_user)
                          index: int = Query(description='Which face in the picture will be used',
                                        default=1, 
                                        ge=1, le=10), 
-                         quality: int = Query(description='Quality to use, Default = Preview',
-                                       default=1),
+                         quality: QualityType = Query(description='Quality to use, Default = Preview',
+                                       default=QualityType.PREVIEW),
                          colorA: ColorExamples = Query(description='First Color to use, Default = Black',
                                                   default=ColorExamples.BLACK),
                          colorB: ColorExamples = Query(description='Last Color to use, Default = Black',
                                                   default=ColorExamples.WHITE),
                          border: Annotated[Union[ColorExamples, None], 
-                                      Query(description='Border Color to use, Default = None')] = None
+                                            Query(description='Border Color to use, Default = None')] = None
                         ):
-    print(current_user)
+    """
+    Endpoint to get a user's picture.
+    
+    This endpoint is used to get a user's picture. It requires authentication and supports file uploads. The user's picture can be customized with various options such as the desired face index, picture quality, color options, and border color.
+    
+    Parameters:
+        - current_user: The current authenticated user.
+        - pic_file: The uploaded picture file.
+        - index: The index of the face in the picture to be used (default: 1).
+        - quality: The quality to use for the picture (default: 1).
+        - colorA: The first color to use for customization (default: ColorExamples.BLACK).
+        - colorB: The last color to use for customization (default: ColorExamples.WHITE).
+        - border: The border color to use for customization (default: None).
+    
+    Returns:
+        - FileResponse: The response containing the user's customized picture.
+    
+    Raises:
+        - HTTPException(status_code=status.HTTP_401_UNAUTHORIZED): If the current user is not active.
+        - HTTPException(status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE): If the uploaded file is not in 'image/jpeg' format.
+        - HTTPException(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE): If the uploaded file size exceeds 15MB.
+        - HTTPException(status_code=status.HTTP_409_CONFLICT): If there is no face detected in the uploaded picture.
+        - HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR): If there is an error processing the request.
+    """
     if current_user.is_active is False:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
                             detail="User disabled, please contact admin")
     if pic_file.content_type not in ("image/jpeg",):
-        raise HTTPException(
-            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-            detail="Unsupported content type. Supported content types are 'image/jpeg'",
-            )
-    if pic_file.size > 1024 * 1024 * 15:     # 15MB
+        raise HTTPException(status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+                            detail="Unsupported content type. Supported content types are 'image/jpeg'",)
+    if pic_file.size > 1024 * 1024 * LIMIT_SIZE_USER:     # 15MB
         raise HTTPException(
             status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
-            detail="File size is too large",
+            detail=f'File size is too large, the limit is {LIMIT_SIZE_USER} MB',
         )
+    
+    newPicture = MakePicture(current_user)
     Acolor = Color.get_color_rgb(colorA.value)
     Bcolor = Color.get_color_rgb(colorB.value)
     BorderColor = None if border is None else Color.get_color_rgb(border.value)
+
     try:
-        # Create a temporary file
-        with tempfile.NamedTemporaryFile(delete=True, suffix=".jpg") as temp_file:
-           # Save the uploaded file to the temporary file
-            with open(temp_file.name, "wb") as f:
-                f.write(await pic_file.read())
-            # print(temp_file.name)
-            faces = BigPic(temp_file.name).get_faces()
-            if(len(faces) == 0):
-                raise HTTPException(
-                    status_code=status.HTTP_409_CONFLICT,
-                    detail= "No Faces detected in the picture 'image/jpeg'",
-                    )
-            # print(len(faces))
-            index -= 1
-            # faces[index].resize(300)
-            faces[index].removeBG()
-            faces[index].addBG(Acolor,Bcolor)
-            faces[index].set_contour()
-            if BorderColor is not None:
-                faces[index].setBorder(BorderColor)
-            faces[index].setBlur(30)
-            faces[index].save(tol= 5)       # save the pic on a temp file during 5 sec
-            return FileResponse(faces[index].get_path())
+        pic_path = await newPicture.make_user_picture(pic_file=pic_file, 
+                                                      Acolor=Acolor, 
+                                                      Bcolor=Bcolor, 
+                                                      BorderColor=BorderColor, 
+                                                      quality=quality, 
+                                                      index=(index-1))
+        return FileResponse(pic_path)
+    except NoFaceException as e:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail= e.message,)
     except Exception as e:
         print(str(e))
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail= "there was an error processing your request. Try again later",
-            )
-
-    return {"filename": pic_file.filename,
-            "format": pic_file.content_type,
-            "size": pic_file.size,
-            "sizeInBytes": round(len(pic_file.file.read())/1024,ndigits=2),
-            "temp_file": str(temp_file.name),}
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail= "there was an error processing your request. Try again later",)
