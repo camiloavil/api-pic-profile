f52af7566babb191bdb95cd4d8b5cbdf8928552f
Camilo Avila <camilo.avil@gmail.com>
2023-07-12 10:49:39 -0500

    fix(DB SQLite whit async metods) fixing the error now async metods can use SQLite

--- app/DB/db.py
+++ app/DB/db.py
@@ -10,13 +10,13 @@ SQLITE_FILENAME = 'DB.sqlite'
 # BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-
 #set location of DB File on the same folder of this file
-DATABASE_URL = f'sqlite:///{os.path.join(BASE_DIR,SQLITE_FILENAME)}' 
+# DATABASE_URL = f'sqlite:///{os.path.join(BASE_DIR,SQLITE_FILENAME)}?check_same_thread=False' 
 # DATABASE_URL = "postgresql://user:password@postgresserver/db"
-
-#echo =True set verbose entire DB process
-engine = create_engine(DATABASE_URL, echo = False)         
+# engine = create_engine(DATABASE_URL, echo = False)  
+engine = create_engine(f'sqlite:///{os.path.join(BASE_DIR,SQLITE_FILENAME)}', 
+                       connect_args={'check_same_thread': False}, 
+                       echo=False)
 
 def create_db_table():
     MyModels.metadata.create_all(engine)
