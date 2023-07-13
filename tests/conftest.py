0f40bc72a6167a5a92e7e98c054b4f1250a1de22
Camilo Avila <camilo.avil@gmail.com>
2023-07-12 20:16:02 -0500

    test(change Tests) set conftest init parameters of future testetin and set class Tests

---
+++ tests/conftest.py
@@ -0,0 +1,77 @@
+# pytest
+import pytest
+# FastAPI
+from fastapi.testclient import TestClient
+# from httpx import AsyncClient
+# APP
+from app.models import MyModels
+from app.models.user import User, UserBase, UserFB, UserUpdate
+from app.security.secureuser import get_password_hash
+from app.DB.db import get_session
+from app.DB.querys import add_user_to_db
+# SQLModel
+from sqlmodel import Session, create_engine
+# Python
+import os
+
+from main import app
+
+#Filename of sqlite DB
+TEST_SQLITE_FILENAME = 'test_DB.sqlite'
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+DATABASE_URL = f'sqlite:///{os.path.join(BASE_DIR,TEST_SQLITE_FILENAME)}' 
+test_engine = create_engine(DATABASE_URL, 
+                            connect_args={'check_same_thread': False}, 
+                            echo = False)
+MyModels.metadata.create_all(test_engine)
+
+def delete_db_file():
+    os.remove(os.path.join(BASE_DIR, TEST_SQLITE_FILENAME))
+
+def get_test_session():
+    with Session(test_engine) as session:
+        yield session
+
+app.dependency_overrides[get_session] = get_test_session
+
+@pytest.fixture(scope="class")
+def client() -> TestClient:
+    # MyModels.metadata.drop_all(test_engine)
+    MyModels.metadata.create_all(test_engine)
+    yield TestClient(app)
+    delete_db_file()
+
+@pytest.fixture()
+def setUp_users():
+    user1 = {
+        "name": "Test user One",
+        "email": "one@example.com",
+        "password": "MyPassword",
+        "city": "TestyCity",
+        "country": "Testland",
+        "userType": "admin"
+        }
+    user2 = {
+        "name": "Test user Two",
+        "email": "two@example.com",
+        "password": "MyPassword",
+        "city": "TestyCity",
+        "country": "Testland",
+        "userType": "test"
+        }
+    user3 = {
+        "name": "Test user Tree",
+        "email": "tree@example.com",
+        "password": "MyPassword",
+        "city": "TreeCity",
+        "country": "Treeland",
+        "userType": "test"
+        }
+    with Session(test_engine) as session:
+        add_user_to_db(User(**user1, pass_hash= get_password_hash(user2["password"])),
+                       session)
+        add_user_to_db(User(**user2, pass_hash= get_password_hash(user2["password"])),
+                       session)
+        add_user_to_db(User(**user3, pass_hash= get_password_hash(user3["password"])),
+                       session)
+    yield user1,user2,user3
---
+++ tests/test_images.py
@@ -0,0 +1,49 @@
+# pytest
+import pytest
+# FastAPI
+from fastapi import status
+from httpx import AsyncClient
+# APP
+from app.models.user import User, UserBase, UserFB, UserUpdate
+# Testing
+from .conftest import app, client
+# Python
+import os
+
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+class TestFreePicture:
+    def test_getFreePicture_whitout_picture(self, client: client):
+        response = client.post("/pictures/example")
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY, "Route whitout Picture"  # noqa: E501
+
+    # @pytest.mark.anyio
+    # def test_getFreePicture(self, client: client):
+    #     # data = {'message': 'Hello, world!'}
+    #     files={'picture_file': open(os.path.join(TEST_DIR,'images/testing.jpg'), 'rb')}
+    #     # response = client.post("/pictures/example", data=data, files=files) 
+    #     # async with AsyncClient(app=app, base_url="http://test") as ac:
+    #     #     response = await ac.post("/pictures/example", files=files) 
+    #     response = client.post("/pictures/example", files=files) 
+    #     # print(response.text)
+    #     print(response.url)
+    #     assert response.status_code == status.HTTP_200_OK, "Picture returned successfully"  # noqa: E501
+    #     # assert response.status_code == status.HTTP_400_BAD_REQUEST, "Picture returned successfully"  # noqa: E501
+
+    @pytest.mark.asyncio
+    async def test_async_getFreePicture(self):
+        files={'picture_file': open(os.path.join(TEST_DIR,'images/testing.jpg'), 'rb')}
+        async with AsyncClient(app=app, base_url="http://test") as ac:
+            response = await ac.post("/pictures/example", files=files) 
+            assert response.status_code == status.HTTP_200_OK, "Picture returned successfully"  # noqa: E501
+            assert response.headers['content-type'] == 'image/png', "Returned content-type is not image/png"  # noqa: E501
+        # This request must create a nee register on DB
+        # print(str(response.))
+        assert response.json() == {"message": "Tomato"}, "Error to print stout"
+
+class TestHome:
+    def test_home_item(self, client: client): 
+        # response = client.get("/items/foo", headers={"X-Token": "coneofsilence"})
+        response = client.get("/")
+        assert response.status_code == status.HTTP_200_OK
